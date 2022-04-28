4ffb56b2068b219b1ca16847f9436283f8fe3b60
arampytoshka <aramsem965@gmail.com>
2022-04-28 16:36:26 +0300

    Изменил текст в volume_question, добавил уличтоку в отдельном файле

---
+++ Coffee-Shop/Snail.py
@@ -0,0 +1,14 @@
+up = 5
+down = 2
+height = 10
+way = 0
+day = 0
+for day in range(100):
+    if way < height:
+        way = way + up
+        if way < height:
+            way = way - down
+    day = day + 1
+    if way >= height:
+        break
+print(day)
